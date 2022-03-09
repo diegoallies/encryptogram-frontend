30ded754eda377ddef7bcbb492f5c8e26059fe6a
Encrypto27 <diegoallies27@gmail.com>
2022-03-09 16:25:59 +0200

    front end is starting to work

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="container">
-    <header class="jumbotron">
-      <h3>{{ content }}</h3>
-    </header>
+  <div class="section-heading space">
+    <h1>Products</h1>
+
+
+ <div v-for="post in posts" :key="post" class="col-md-6">
+                <h5>{{ post.postText }}</h5>
+              </div>
+             
+
   </div>
+
+       
+ 
+ 
 </template>
+
 <script>
-import UserService from "../services/user.service";
 export default {
-  name: "Home",
   data() {
     return {
-      content: "",
+      posts: [],
     };
   },
   mounted() {
-    UserService.getPublicContent().then(
-      (response) => {
-        this.content = response.data;
-      },
-      (error) => {
-        this.content =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
-      }
-    );
+    fetch("https://encryptogram-backend.herokuapp.com/posts/")
+      .then((res) => res.json())
+      .then((data) => {
+        this.posts = data;
+        console.log(data, this.posts);
+      });
   },
+
 };
 </script>
