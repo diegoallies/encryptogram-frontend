c1683c0d3a892899b719913a78cd43fe2e124357
Encrypto27 <diegoallies27@gmail.com>
2022-03-25 15:06:20 +0200

    push new code

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -24,7 +24,31 @@
       <span class="lmt">{{currentUser._id}}</span>
     </p>
       <button @click="deleteUser(currentUser._id)" class="glow-on-hover btn-block btnn thb">Delete Account</button>
-  <button @click="deleteUser(currentUser._id)" class="glow-on-hover btn-block btnn lhb">Edit Account Details</button>
+      <button @click="editUser(currentUser._id)" class="glow-on-hover btn-block btnn lhb" data-toggle="modal" data-target="#exampleModal">Edit Account Details</button>
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       <input type="text" id="inputt" :placeholder="currentUser.fullname" class="btn-block" v-model="fullname"> 
+       <input type="text" id="inputt" :placeholder="currentUser.email" class="btn-block" v-model="email"> 
+       <input type="text" id="inputt" :placeholder="currentUser.number" class="btn-block" v-model="number"> 
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal" @click="editUser(currentUser._id)">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
     
   </div>
@@ -52,6 +76,24 @@ export default {
         this.$store.dispatch('auth/logout');
         this.$router.push('/login');
     },
+    editUser(id){
+    fetch("https://encryptogram-backend.herokuapp.com/users/" + id, {
+        method: "PATCH",
+        body: JSON.stringify({
+
+        fullname: this.fullname,
+        email: this.email,
+        number: this.phone_number,
+          
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Authorization: `Bearer ` + this.currentUser.accessToken
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    },
   },
   name: 'Profile',
   computed: {
@@ -63,7 +105,15 @@ export default {
     if (!this.currentUser) {
       this.$router.push('/login');
     }
+
+    fetch("https://encryptogram-backend.herokuapp.com/users/")
+      .then((res) => res.json())
+      .then((data) => {
+        this.users = data;
+        console.log(data, this.users);
+      });
   }
+ 
 };
 </script>
 
